c5167efc3172cb2518385fb6e2e51ac0ac7ad657
huangxinhere <1159390360@qq.com>
2020-10-17 12:24:13 +0800

    test

---
+++ ti 2 - 副本.cpp
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int main()
+{
+    int y;
+	printf("input y\n");
+	scanf("%d",&y);
+	if (y>0)
+	{
+		printf("大于");
+	 }  
+     else if(y<0)
+    {
+    	printf("小于");
+	 } 
+	 else
+	 {
+	 	printf("等于");
+	 }
+	 
+	 
+	 
+     return 0;
+} 
